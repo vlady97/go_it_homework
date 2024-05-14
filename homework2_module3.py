41a1cced35a9a0b68d892139fe66b163cd2627a0
vladyslavadanivska <102226180+vlady97@users.noreply.github.com>
2024-05-14 12:09:28 +0200

    Add files via upload

---
+++ homework2_module3.py
@@ -0,0 +1,12 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    if not (1 <= min <= max <= 1000) or quantity <= 0:
+        return []
+        
+    unique_numbers = set()
+    
+    while len(unique_numbers) < quantity:
+      unique_numbers.add(random.randint(min, max))
+    
+    return sorted(unique_numbers)

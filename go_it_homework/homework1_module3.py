221004e94aef4cdc9b2c2d24bb428173802b745a
vladyslavadanivska <102226180+vlady97@users.noreply.github.com>
2024-05-31 01:07:02 +0200

    Add files via upload

---
+++ go_it_homework/homework1_module3.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+def is_valid_date_format(date_string):
+   try: 
+      datetime.strptime(date_string, "%Y-%m-%d")
+      return True
+   except ValueError:
+      return False
+   
+
+def get_days_from_today(date):
+   if not is_valid_date_format(date):
+      return "Неправильний формат дати."
+   
+   input_date = datetime.strptime(date, "%Y-%m-%d").date()
+   today = datetime.today().date()
+   days_difference = (input_date - today).days
+   return days_difference
